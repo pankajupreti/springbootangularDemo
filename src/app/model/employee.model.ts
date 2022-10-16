054da154debab14e21375190f14c4e583daa26ed
pankajupreti <pankaj.upreti@hotmail.com>
2022-10-16 17:48:54 +0530

    Angular code

---
+++ src/app/model/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee{
+    id:number;
+    firstName:String;
+    lastName:String;
+    email:String;
+    phone:String;
+    hireDate:Date;
+    salary:number;
+    managerId:number;
+    department:number;
+}
